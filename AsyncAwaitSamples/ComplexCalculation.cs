defd7f605b0218c53700fd91f9d229bdfa130e49
Arun Thomas <ArunThomas04@gmail.com>
2020-04-01 15:58:31 +0530

    Add project files.

---
+++ AsyncAwaitSamples/ComplexCalculation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitSamples
+{
+    public class ComplexCalculation
+    {
+
+        public int DoComplexCalculation()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            while(true)
+            {
+                if(watch.ElapsedMilliseconds > 5000)
+                {
+                    break;
+                }
+            }
+
+            return 10;
+        }
+    }
+}
---
+++ AsyncAwaitSamples/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitSamples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var op1 = CallComplexOperation();
+            var op2 = CallComplexOperation();
+            var op3 = CallComplexOperation();
+            var op4 = CallComplexOperation();
+
+            await Task.WhenAll(op1, op2, op3, op4);
+
+            int sum = op1.Result + op2.Result + op3.Result + op4.Result;
+            MessageBox.Show(sum.ToString());
+        }
+
+        private  Task<int> CallComplexOperation()
+        {
+          
+            return Task.Run(() =>
+            {
+                var complexCalc = new ComplexCalculation();
+                return  complexCalc.DoComplexCalculation();
+            });            
+        }
+    }
+}
